a21a1ddcf64b7e51f6a75b5089c4a6c5d069bbb9
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-05-01 22:08:45 +0100

    set up lab 2

---
+++ lab-2/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  project = "qwiklabs-gcp-02-6190917e2de0"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
