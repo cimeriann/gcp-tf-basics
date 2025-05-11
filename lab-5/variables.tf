3ef0ce76d0cd88d386b237d448da651094e80c94
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2025-05-11 22:12:27 +0100

    variables

---
+++ lab-5/variables.tf
@@ -0,0 +1,12 @@
+variable "region"{
+    description = "The default region for our project"
+    default     = "us-west1"
+}
+variable "zone"{
+    description = "The default zone for this lab"
+    default     = "us-west1-a"
+}
+variable project_id {
+    description = "my project id"
+    default     = "qwiklabs-gcp-04-07a5ce07158c"
+}
